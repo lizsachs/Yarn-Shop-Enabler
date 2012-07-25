fa34b93930b888eba8bd5112266c4e98a9ecf902
lizsachs <liz.gifford@gmail.com>
2012-07-24 22:51:00 -0400

    refactoring service class

--- grails-app/services/rav/CalculateService.groovy
+++ grails-app/services/rav/CalculateService.groovy
@@ -4,23 +4,38 @@ class CalculateService {
 
     def httpService
 
+    def initializeYarnWeightCounts = [
+            'Thread':0,
+            'Cobweb':0,
+            'Lace':0,
+            'Light Fingering':0,
+            'Fingering':0,
+            'Sport':0,
+            'DK':0,
+            'Worsted':0,
+            'Aran / Worsted':0,
+            'Aran':0,
+            'Bulky':0,
+            'Super Bulky':0,
+            'Other':0,
+            'No Yarn Specified':0];
+
+    def initializeYardageMap = [
+            ['label':'0-150','count':0,'min':0,'max':149],
+            ['label':'150-300','count':0,'min':150,'max':299],
+            ['label':'300-450','count':0,'min':300,'max':449],
+            ['label':'450-600','count':0,'min':450,'max':599],
+            ['label':'600-750','count':0,'min':600,'max':749],
+            ['label':'750-900','count':0,'min':750,'max':899],
+            ['label':'900-1200','count':0,'min':900,'max':1199],
+            ['label':'1200-1500','count':0,'min':1200,'max':1499],
+            ['label':'1500-1800','count':0,'min':1500,'max':1799],
+            ['label':'1800-2100','count':0,'min':1800,'max':2099],
+            ['label':'2100+','count':0,'min':2100,'max':null]
+    ];
+
     // I put all of this in one service call to minimize calls to the database rather than having separate calculations for each type of data, only need to iterate through projects once.
     def countProjectDetails(userName, allProjects, token) {
-        def initializeYarnWeightCounts = [
-                'Thread':0,
-                'Cobweb':0,
-                'Lace':0,
-                'Light Fingering':0,
-                'Fingering':0,
-                'Sport':0,
-                'DK':0,
-                'Worsted':0,
-                'Aran / Worsted':0,
-                'Aran':0,
-                'Bulky':0,
-                'Super Bulky':0,
-                'Other':0,
-                'No Yarn Specified':0];
 
         def yarnWeightCount = ['All':initializeYarnWeightCounts.clone()];
         def patternTypeCount = ['No Pattern Type Specified':0];
@@ -28,16 +43,14 @@ class CalculateService {
 
         allProjects.each{
             def project = httpService.getProjectDetails(userName,it.id, token);
+            def yarnYardage = project.project.packs.total_yards;
             def patternType = 'No Pattern Type Specified';
             totalPatternTypes++;
             if(it.pattern_id){
                 def pattern = httpService.getPatternDetails(it.pattern_id,token);
                 patternType = pattern ? pattern.pattern.pattern_type.name : patternType;
-                if(patternType && patternTypeCount[patternType]){
-                    patternTypeCount[patternType]++;
-                }
-                else if(patternType){
-                    patternTypeCount[patternType] = 1;
+                if(patternType) {
+                    patternTypeCount[patternType] = patternTypeCount.get(patternType, 0) + 1;
                 }
             }
             else{
@@ -52,9 +65,9 @@ class CalculateService {
                         uniqueYarnsPerProject.add(it.yarn_weight.name);
                     }
                 }
-                if(!yarnWeightCount[patternType]){
-                    yarnWeightCount[patternType] = initializeYarnWeightCounts.clone();
-                }
+
+                yarnWeightCount[patternType] = yarnWeightCount.get(patternType,initializeYarnWeightCounts.clone());
+
                 uniqueYarnsPerProject.each{
                     yarnWeightCount[patternType][it]++;
                     yarnWeightCount['All'][it]++;
